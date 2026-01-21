217ecfadffeb32ee5683903f23bbf39554b98e31
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-01-21 14:58:02 -0500

    28. Cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,10 +1,14 @@
 const CartItem = ({item}) => {
   return (
-    <div className="p-2 hover:bg-[#DAFFA2]">
+    <div className="flex cursor-pointer space-x-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
         <img className="h-24" src={item.src}/>
-        <div>{item.title}</div>
-        <div>{item.description}</div>
-        <div>{item.price}</div>
+        <div className="space-y-2">
+            <div className="font-bold">{item.title}</div>
+            <div className="text-sm text-gray-400">
+                {item.description}
+            </div>
+        </div>
+        <div className="font-bold">${item.price}</div>
     </div>
   )
 }
