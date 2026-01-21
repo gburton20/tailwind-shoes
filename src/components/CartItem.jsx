df32332ef7c9207a1298856cc82c77a1101d4162
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-01-21 15:56:03 -0500

    30. Tailwind merge (tw-merge)

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -23,17 +23,25 @@ const CartItem = ({item}) => {
             <div className="flex space-x-6">
                 <div>
                     <div className="font-bold">SIZE</div>
-                    <Select title="" options={SIZES}/>
+                    <Select 
+                        title="" 
+                        options={SIZES} 
+                        className={"w-16 p-1"}
+                    />
                 </div>
 
                 <div>
                     <div className="font-bold">QTY</div>
-                    <Select title="" options={QTY}/>
+                    <Select 
+                        title="" 
+                        options={QTY}
+                        className={"w-16 p-1"}
+                    />
                 </div>
+            </div>
                 <button>
                     <CiTrash size={25} className="text-black"/>
                 </button>
-            </div>
         </div>
     </div>
   )
