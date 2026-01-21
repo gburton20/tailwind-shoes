8b255ee9887dafd4c4815760661c054659e9f5a9
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-01-21 12:02:26 -0500

    26. Card grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,9 @@ export function Card ({item}) {
     <div className={`${item.className} max-w-xl cursor-pointer transform transition hover:scale-105`}>
         <div className="p-8">
             <div className="text-2xl font-bold">{item.title}</div>
-            <div className="underline underline-offset-4 font-semibold mt-10">SHOP NOW + </div>
+            <div className="underline underline-offset-4 font-semibold mt-10">
+              SHOP NOW + 
+            </div>
         </div>
         <img 
           className="absolute left-[50%] top-5 h-40" 
