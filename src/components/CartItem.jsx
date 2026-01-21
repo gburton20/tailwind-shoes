0eed1dbf832cdae2f610b5f35ae394dd5c48f98f
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-01-21 15:43:20 -0500

    29. Cart item inputs

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,14 +1,40 @@
+import { CiTrash } from "react-icons/ci"
+import { Select } from "./Select"
+import { SIZES, QTY } from "../constant"
+
 const CartItem = ({item}) => {
   return (
-    <div className="flex cursor-pointer space-x-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
-        <img className="h-24" src={item.src}/>
-        <div className="space-y-2">
-            <div className="font-bold">{item.title}</div>
-            <div className="text-sm text-gray-400">
-                {item.description}
+    <div className="cursor-pointer bg-gray-50 p-2 hover:bg-[#DAFFA2] space-y-2">
+        <div className="flex space-x-2">
+            {/* Image: */}
+            <img className="h-24" src={item.src}/>
+            <div className="space-y-2">
+                {/* Title and description */}
+                <div className="font-bold">{item.title}</div>
+                <div className="text-sm text-gray-400">
+                    {item.description}
+                </div>
+            </div>
+            {/* Price: */}
+            <div className="font-bold">${item.price}</div>
+        </div>
+
+        <div className="flex justify-between pl-32">
+            <div className="flex space-x-6">
+                <div>
+                    <div className="font-bold">SIZE</div>
+                    <Select title="" options={SIZES}/>
+                </div>
+
+                <div>
+                    <div className="font-bold">QTY</div>
+                    <Select title="" options={QTY}/>
+                </div>
+                <button>
+                    <CiTrash size={25} className="text-black"/>
+                </button>
             </div>
         </div>
-        <div className="font-bold">${item.price}</div>
     </div>
   )
 }
